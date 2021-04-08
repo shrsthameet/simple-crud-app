890b3f5dcfa87ad64ac7fac8d49b3e6661a2c051
Ameet Shrestha <shrsth.ameet@gmail.com>
2021-04-09 00:56:12 +0545

    Fix: removed redux devtool

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -2,7 +2,6 @@ import {rootReducer} from "./rootReducer";
 import {createStore, compose, applyMiddleware} from "redux";
 import thunk from "redux-thunk";
 
-const store = createStore(rootReducer, compose(applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+const store = createStore(rootReducer, compose(applyMiddleware(thunk)));
 
 export default store;
